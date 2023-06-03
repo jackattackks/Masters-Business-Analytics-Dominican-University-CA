d352d6ecadc9d77bdd38c7bfe9793b4e48550043
Jackie Ocaña <jacquelineocana@my.smccd.edu>
2023-06-03 09:22:05 -0700

    update in class

--- MSBA5508-Marketing-Analytics/June3/in-class/percep.R
+++ MSBA5508-Marketing-Analytics/June3/in-class/percep.R
@@ -20,4 +20,27 @@ brand.sc[,1:9]<-data.frame(scale(brand.ratings[,1:9]))
 library(corrplot)
 corrplot(cor(brand.sc[,1:9]), order = "hclust")
 
-         
+brand.mean <- aggregate(.~brand,data = brand.sc,mean)         
+
+rownames(brand.mean)<-brand.mean[,1]
+brand.mean<-brand.mean[,-1]
+brand.mean
+
+#install.packages("RColorBrewer")
+#install.packages("gplots")
+
+library(gplots)
+library(RColorBrewer)
+
+heatmap(as.matrix(brand.mean), col = brewer.pal(9, "GnBu"), trace = "none", 
+        key = FALSE, dendrogram = "none", main="\n\n\n\n\nBrand Attributes")
+
+brand.pc<-prcomp(brand.sc[,1:9])
+summary(brand.pc)
+
+biplot(brand.pc)
+
+brand.mu.pc<-prcomp(brand.mean,scale=TRUE)
+summary(brand.mu.pc)
+
+biplot(brand.mu.pc)
